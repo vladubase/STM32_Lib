aff374350989e9b0f0272efcc7a15e0248e61ef2
vladubase <vladubase@gmail.com.com>
2021-01-10 22:33:55 +0300

    update: RCC config file.

--- STM32F0/Setup files/STM32F0_SWD/Objects/main.d
+++ STM32F0/Setup files/STM32F0_SWD/Objects/main.d
@@ -2,10 +2,10 @@
 .\objects\main.o: .\Code\inc\main.h
 .\objects\main.o: .\CMSIS\inc\stm32f0xx.h
 .\objects\main.o: .\CMSIS\inc\core_cm0.h
-.\objects\main.o: C:\User Programs\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: D:\User Programs\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\main.o: .\CMSIS\inc\core_cmInstr.h
 .\objects\main.o: .\CMSIS\inc\core_cmFunc.h
 .\objects\main.o: .\CMSIS\inc\system_stm32f0xx.h
-.\objects\main.o: C:\User Programs\Keil\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\main.o: D:\User Programs\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
 .\objects\main.o: .\Code\inc\InitRCC.h
 .\objects\main.o: .\Code\inc\InitSWD.h
