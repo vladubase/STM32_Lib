c70af9e7cc8c7a3c9214f8ec6ac1dae4f8413a9f
vladubase <vladubase@gmail.com>
2021-10-26 10:29:29 +0300

    Update ADC init.

--- STM32F4/Setup files/STM32F4_ADC/Code/inc/main.h
+++ STM32F4/Setup files/STM32F4_ADC/Code/inc/main.h
@@ -30,16 +30,37 @@
 
 /*********************************** Defines **********************************/
 
-
+__IO uint32_t SysTick_CNT = 0;						// SysTick count.
 
 /********************************** Functions *********************************/
 
 void InitSystem (void) {
-	SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2));	// Enable Hardware Floating Point.
+	// Enable Hardware Floating Point.
+	SCB->CPACR |= ((3UL << 10*2) | (3UL << 11*2));
+	
+	// SysTick.
+	SysTick->LOAD &= ~SysTick_LOAD_RELOAD_Msk;
+	SysTick->LOAD = f_SYSCLK / (1000 - 1);			// 100 MHz / 1000 -> get ms.
+	SysTick->VAL &= ~SysTick_VAL_CURRENT_Msk;
+	SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk |	// Processor clock (AHB).
+		SysTick_CTRL_TICKINT_Msk |					// Asserts the SysTick exception request.
+		SysTick_CTRL_ENABLE_Msk;					// Counter enabled.
 }
 
 void delay_ms (uint32_t time) {
-	while (time--);
+	SysTick->VAL &= ~SysTick_VAL_CURRENT_Msk;
+	SysTick->VAL = f_SYSCLK / (1000 - 1);
+	SysTick_CNT = time;
+	while (SysTick_CNT);
+}
+
+
+/********************************** Interrupts ********************************/
+
+void SysTick_Handler (void) {
+	if (SysTick_CNT > 0) {
+		SysTick_CNT--;
+	}
 }
 
 #endif /* _MAIN_H_ */
